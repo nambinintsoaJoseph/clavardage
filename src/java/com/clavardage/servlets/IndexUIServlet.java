bbd509061da2af3647d80edb3c0bcea2c1ce63db
RAZANAKANAMBININTSOA Joseph <raznambinintsoa3@gmail.com>
2025-01-28 06:52:57 +0100

    Servlet pour la requete /

---
+++ src/java/com/clavardage/servlets/IndexUIServlet.java
@@ -0,0 +1,16 @@
+package com.clavardage.servlets;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class IndexUIServlet extends HttpServlet
+{
+    @Override 
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException 
+    {
+        this.getServletContext().getRequestDispatcher("/WEB-INF/index.jsp").forward(request, response);
+    }
+}
