b70e3025d7e494f1c29d3b1c3b8fe70a686a0050
RAZANAKANAMBININTSOA Joseph <raznambinintsoa3@gmail.com>
2025-02-01 09:31:51 +0100

    Refactoring UtilisateurDAO.java

--- src/java/com/clavardage/dao/UtilisateurDAO.java
+++ src/java/com/clavardage/dao/UtilisateurDAO.java
@@ -15,6 +15,38 @@ import java.util.List;
 
 public class UtilisateurDAO 
 {
+    private final Jdbc jdbc = new Jdbc();
+    
+    /**
+     * Exécute une requête SQL
+     * 
+     * @param sql La requête sql que l'on souhaite exécuter.
+     * @param params Les paramètres à insérer dans une requête preparée.
+     * 
+     * @throws ClassNotFoundException 
+     * @throws SQLException
+     */
+    private void executeSQLCommand(String sql, Object... params) throws ClassNotFoundException, SQLException
+    {
+        try(Connection connection = jdbc.getConnection();
+            PreparedStatement stmt = connection.prepareStatement(sql))
+        {
+            for(int i = 0; i < params.length; i++)
+            {
+                stmt.setObject(i+1, params[i]);
+            }
+            stmt.executeUpdate();
+        }
+        catch(ClassNotFoundException ex)
+        {
+            ex.printStackTrace();
+        }
+        catch(SQLException ex)
+        {
+            ex.printStackTrace();
+        }
+    }
+    
     /**
      * Permet d'ajouter un utilisateur
      * 
@@ -25,28 +57,9 @@ public class UtilisateurDAO
      */
     public void addUtilisateur(Utilisateur utilisateur) throws ClassNotFoundException, SQLException 
     {
-        Jdbc jdbc = new Jdbc(); 
         String sql = "INSERT INTO utilisateur(nom, prenom, date_naissance, email, mot_passe) VALUES(?, ?, ?, ?, ?)";
-        
-        try (Connection connection = jdbc.getConnection(); PreparedStatement stmt = connection.prepareStatement(sql))
-        {
-            stmt.setString(1, utilisateur.getNom());
-            stmt.setString(2, utilisateur.getPrenom());
-            stmt.setString(3, utilisateur.getDate_naissance());
-            stmt.setString(4, utilisateur.getEmail());
-            stmt.setString(5, utilisateur.getMot_passe());
-     
-            System.out.println(stmt.toString());
-            stmt.executeUpdate();
-        } 
-        catch(ClassNotFoundException ex) 
-        {
-            ex.printStackTrace();
-        }
-        catch(SQLException ex) 
-        {
-            ex.printStackTrace();
-        }
+        executeSQLCommand(sql, utilisateur.getNom(), utilisateur.getPrenom(), utilisateur.getDate_naissance(), 
+                utilisateur.getEmail(), utilisateur.getMot_passe());
     }
     
     /**
@@ -59,35 +72,13 @@ public class UtilisateurDAO
      */
     public void updateUtilisateur(Utilisateur utilisateur) throws ClassNotFoundException, SQLException 
     {
-        Jdbc jdbc = new Jdbc(); 
         String sql = "UPDATE utilisateur SET nom = ?, prenom = ?, date_naissance = ?, "
                 + "email = ?, mot_passe = ?, residence = ?, info_scolaire = ?, "
                 + "info_professionnel = ?, photo_profil = ? WHERE id_utilisateur = ?";
-        
-        try (Connection connection = jdbc.getConnection(); PreparedStatement stmt = connection.prepareStatement(sql))
-        {
-            stmt.setString(1, utilisateur.getNom());
-            stmt.setString(2, utilisateur.getPrenom());
-            stmt.setString(3, utilisateur.getDate_naissance());
-            stmt.setString(4, utilisateur.getEmail());
-            stmt.setString(5, utilisateur.getMot_passe());
-            stmt.setString(6, utilisateur.getResidence());
-            stmt.setString(7, utilisateur.getInfo_scolaire());
-            stmt.setString(8, utilisateur.getInfo_professionnel());
-            stmt.setString(9, utilisateur.getPhoto_profil());
-            stmt.setInt(10, utilisateur.getId_utilisateur());
-            
-            System.out.println(stmt);
-            stmt.executeUpdate();
-        } 
-        catch(ClassNotFoundException ex) 
-        {
-            ex.printStackTrace();
-        }
-        catch(SQLException ex) 
-        {
-            ex.printStackTrace();
-        }
+        executeSQLCommand(sql, utilisateur.getNom(), utilisateur.getPrenom(),
+                utilisateur.getDate_naissance(), utilisateur.getEmail(), utilisateur.getMot_passe(),
+                utilisateur.getResidence(), utilisateur.getInfo_scolaire(), utilisateur.getInfo_professionnel(), 
+                utilisateur.getPhoto_profil(), utilisateur.getId_utilisateur());
     }
     
     /**
@@ -99,22 +90,8 @@ public class UtilisateurDAO
      */
     public void deleteUtilisateur(Utilisateur utilisateur) throws ClassNotFoundException, SQLException 
     {
-        Jdbc jdbc = new Jdbc(); 
         String sql = "DELETE FROM utilisateur WHERE id_utilisateur = ?";
-        
-        try (Connection connection = jdbc.getConnection(); PreparedStatement stmt = connection.prepareStatement(sql))
-        {
-            stmt.setInt(1, utilisateur.getId_utilisateur());
-            stmt.executeUpdate();
-        } 
-        catch(ClassNotFoundException ex) 
-        {
-            ex.printStackTrace();
-        }
-        catch(SQLException ex) 
-        {
-            ex.printStackTrace();
-        }
+        executeSQLCommand(sql, utilisateur.getId_utilisateur());
     }
     
     /**
@@ -128,7 +105,6 @@ public class UtilisateurDAO
      */
     public Utilisateur findUtilisateurById(int idUtilisateur) throws ClassNotFoundException, SQLException 
     {
-        Jdbc jdbc = new Jdbc();
         Utilisateur utilisateur = new Utilisateur(); 
         String sql = "SELECT * FROM utilisateur WHERE id_utilisateur = ?";
         
@@ -180,7 +156,7 @@ public class UtilisateurDAO
         Jdbc jdbc = new Jdbc();
         List<Utilisateur> utilisateurs = new ArrayList<Utilisateur>();
         String sql = "SELECT id_utilisateur, nom, prenom, residence, photo_profil "
-                + "FROM utilisateur WHERE nom = ? OR prenom = ?";
+                + "FROM utilisateur WHERE LOWER(nom) = LOWER(?) OR LOWER(prenom) = LOWER(?)";
         
         try (Connection connection = jdbc.getConnection(); PreparedStatement stmt = connection.prepareStatement(sql))
         {
@@ -223,7 +199,6 @@ public class UtilisateurDAO
      */
     public boolean authentificateUtilisateur(String email, String mot_passe) throws ClassNotFoundException, SQLException
     {
-        Jdbc jdbc = new Jdbc();
         String sql = "SELECT id_utilisateur, email, mot_passe FROM utilisateur "
                 + "WHERE email = ? and mot_passe = ?";
         
@@ -232,13 +207,9 @@ public class UtilisateurDAO
             stmt.setString(1, email);
             stmt.setString(2, mot_passe);
             ResultSet utilisateursAffecte = stmt.executeQuery();
-            int nombreUtilisateurAffecte = 0; 
             
-            while(utilisateursAffecte.next())
-                nombreUtilisateurAffecte++;
-            
-            if(nombreUtilisateurAffecte > 0)
-                return true; 
+            if(utilisateursAffecte.next())
+                return utilisateursAffecte.getInt(1) > 0;
         }
         catch(ClassNotFoundException ex) 
         {
